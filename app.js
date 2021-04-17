2ac1cab64741006e062e1dd88522e918e8cd0bae
Ruchiket Borse <RuchiketBorse1@gmail.com>
2021-04-17 19:18:04 +0530

    all changes saved

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const allInputs = document.querySelectorAll(".all-inputs");
 const nameInput = document.querySelector(".name");
 const authorInput = document.querySelector(".author");
-const statusDiv = document.querySelectorAll(".status-div");
+const statusBtn = document.getElementsByClassName("staus-btn");
+const btn = document.getElementsByTagName("button");
 const submitBtn = document.querySelector(".submit-btn");
 const library = document.querySelector(".library");
 
 submitBtn.addEventListener("click", addBook);
-//   FOR STATUS BUTTON
 
 function addBook(e) {
   //MAKING A DIV
@@ -17,19 +17,76 @@ function addBook(e) {
   // FOR SOME STYLING
   libraryDiv.classList.add("library-div");
 
+  // ADD DECORATIVE IMAGE
+  let Image;
+  Image = document.createElement("div");
+  libraryDiv.appendChild(Image);
+  Image.classList.add("Image");
   // FOR NAME OF BOOK
   let Name = document.createElement("H4");
   Name.innerText = nameInput.value;
   //ADD TO DIV
   libraryDiv.appendChild(Name);
   console.log(libraryDiv);
+  Name.classList.add("nameInput");
 
   //FOR AUTHOR OF BOOK
   let Author = document.createElement("H4");
   Author.innerText = authorInput.value;
   //ADD TO DIV
   libraryDiv.appendChild(Author);
-  console.log(libraryDiv);
+  Author.classList.add("authorInput");
 
-  //TO CLEAR THE VALUE OF ALL INPUTS AFTER SUBMITING
+  // MAKE STATUS BUTTONS
+  //reading
+  let Reading = document.createElement("button");
+  Reading.innerText = "Reading";
+  Reading.classList.toggle("Reading");
+  libraryDiv.appendChild(Reading);
+  //add function when click
+  Reading.addEventListener("click", function () {
+    WantList.classList.toggle("ReadingPressed");
+    Completed.classList.toggle("ReadingPressed");
+  });
+
+  //Want-List
+  let WantList = document.createElement("button");
+  WantList.innerText = "Want-List";
+  WantList.classList.toggle("Want-List");
+  libraryDiv.appendChild(WantList);
+
+  WantList.addEventListener("click", function () {
+    Reading.classList.toggle("ReadingPressed");
+    Completed.classList.toggle("ReadingPressed");
+  });
+
+  //Completed
+  let Completed = document.createElement("button");
+  Completed.innerText = "Completed";
+  Completed.classList.toggle("Completed");
+  libraryDiv.appendChild(Completed);
+
+  Completed.addEventListener("click", function () {
+    WantList.classList.toggle("ReadingPressed");
+    Reading.classList.toggle("ReadingPressed");
+  });
+
+  //To Clear the value of input
+  nameInput.value = "";
+  authorInput.value = "";
 }
+
+// function Reading() {
+//   console.log("You Read!!");
+//   let tag = document.createElement("H5");
+//   TimeRanges.innerText = "Reading";
+//   tag.classList.toggle("Reading");
+//   libraryDiv.appendChild(tag);
+// }
+
+//LOCAL STORAGE
+localStorage.setItem("name", "jay");
+
+//for getting
+let x = localStorage.getItem("name");
+console.log(x);
